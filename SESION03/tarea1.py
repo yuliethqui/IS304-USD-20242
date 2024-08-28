f8d468faaf18745f597b770a9fcfca503e05e612
hmurcia <42268927+hmurcia@users.noreply.github.com>
2024-08-28 08:38:50 -0500

    Creacion de la tarea
    Asignacion de la tarea en clase para los estudiantes

---
+++ SESION03/tarea1.py
@@ -0,0 +1,8 @@
+'''
+Tarea para antes de la  próxima clase:
+Crear un programa en Python que cree una clase denominada CuentaBancaria. Agregar encapsulamiento y sobrecarga del constructor de clase, asi como los métodos get y set requeridos para gestionar los atributos de dicha clase.
+Los atributos de la clase debern ser: __numeroCta, __nombreCliente, __fechaApertura, __saldo.
+Agregar metodos para aperturar cuentas, realizar consignaciones y retiros controlados (es decir, no permitir consignaciones negativas, ni retiros superiores al saldo, las aperturas deben exigir un valor inicial mínimo de 100 mil pesos).
+Crear un menú para crear objetos y realizar las diversas operaciones referidas.
+
+'''
